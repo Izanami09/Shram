ff85a3c220698722304ff2bfbbcb77f96b3a717a
Hoa Tran <hoatran@Izanamis-MacBook-Pro.local>
2024-01-19 17:29:29 +0545

    Initial commit

---
+++ app/src/main/java/com/example/shram/ui/presenation/loginscreen/LoginScreen.kt
@@ -0,0 +1,9 @@
+package com.example.shram.ui.presenation.loginscreen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LoginScreen() {
+    Text(text = "This is login screen")
+}
---
+++ app/src/main/java/com/example/shram/ui/presenation/shramscreen/ShramScreen.kt
@@ -0,0 +1,3 @@
+package com.example.shram.ui.presenation.shramscreen
+
+
